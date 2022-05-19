687727dd95ea2c5f14db65902ec3683e5b48e011
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-19 00:07:27 -0500

    Update TokenTimeLock.sol

--- TokenTimeLock.sol
+++ TokenTimeLock.sol
@@ -7,14 +7,22 @@ contract TokenTimeLock {
   address public beneficiary;
   uint public releaseTime;
 
-  constructor (Token _token, address _beneficiary, _releaseTime) public {
+  constructor (Token _token, address _beneficiary, uint _releaseTime) public {
     require(_releaseTime > block.timestamp);
+
     token = _token;
     beneficiary = _beneficiary;
     releaseTime = _releaseTime;
   }
 
   function release() public {
+    require(block.timestamp >= releaseTime);
+
     uint amount = token.balanceOf(address(this));
+
+    require(amount > 0); // Amount can't be 0.
+
+    token.transfer(beneficiary, amount);
+
   }
 }
